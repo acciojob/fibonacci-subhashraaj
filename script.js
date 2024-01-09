0a53e9e9ffc912844110987576bfa28937da4c2e
subhashraaj <150801940+subhashraaj@users.noreply.github.com>
2024-01-09 09:14:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function fibonacci(num) {
-// your code here
+function fibonacci(num){
+	let n1 = 0, n2 = 1, arrived = false ;
+	if(num == 0) return 0;
+	for (let i=2;i<=num;i++){
+		if(i+1==num){
+			arrived = true;
+		}
+		//console.log(n2);
+		let next = n1 + n2;
+		n1 = n2;
+		n2 = next; 
+		if(arrived) return n2;
+	}
+
 }
 
 module.exports = fibonacci;
